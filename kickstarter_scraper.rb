893cc3f999fc831101ed62c863de300cec75a652
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-28 21:05:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,6 +18,7 @@ kickstarter.css("li.project.grid_4").each do |project|
 
 end
 projects
+end
 # projects: kickstarter.css("li.project.grid_4")
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
