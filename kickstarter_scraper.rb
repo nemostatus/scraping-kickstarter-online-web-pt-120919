18ec0d1eed24e77fbc7365264582368e3f809ded
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-28 20:52:44 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,7 +7,7 @@ def create_project_hash
 kickstarter = Nokogiri::HTML(html)
 projects = {}
 
-kickstarter.css("li.project.grid_4").each do 
+kickstarter.css("li.project.grid_4").each do |project|
 
 end
 create_project_hash
