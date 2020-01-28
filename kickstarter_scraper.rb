c4b149700a2a291e71cb5f2e352cea1d59773e30
Matthew Corralejo <mattcorralejo1@gmail.com>
2020-01-28 20:53:43 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,7 @@ projects = {}
 
 kickstarter.css("li.project.grid_4").each do |project|
   title = project.css("h2.bbcard_name strong a").text 
-  projects[title]
+  projects[title] = {
 
 end
 create_project_hash
